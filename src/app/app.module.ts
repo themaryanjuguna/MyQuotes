3d1f835e1d9436a48218cda386577abfbd21ef6e
themaryanjuguna <themaryanjuguna@gmail.com>
2022-04-09 10:56:53 +0300

    form style added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyQuotesComponent } from './my-quotes/my-quotes.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyQuotesComponent,
+    QuoteFormComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-quotes/my-quotes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-quotes',
+  templateUrl: './my-quotes.component.html',
+  styleUrls: ['./my-quotes.component.css']
+})
+export class MyQuotesComponent implements OnInit {
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
+
+
+
